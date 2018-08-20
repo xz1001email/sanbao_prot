e4129c4d82bf54d7abee322b11ac769bf51b1770
xiaozhao <xz1001e_mail@163.com>
2018-08-20 16:38:24 +0800

    build with git commit_tag

--- build.sh
+++ build.sh
@@ -1,9 +1,44 @@
+#!/bin/bash
 
-mkdir -p arm
-cd arm
-if [ "$1" == "a" ] ; then
-../adas_cmake.sh
+
+function git_reversion()
+{
+    git_version=`git rev-parse --verify --short HEAD`
+    if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        git_version+=""
+    else
+        git_version+="_M"
+    fi
+    echo -n "${git_version}"
+}
+
+dms_target="DsmProt"
+adas_target="AdasProt"
+output_folder="output/"
+
+device=$1
+target=wsi
+buildpath="arm/"
+buildtarget=$buildpath"bin/"$target
+
+mkdir -p $buildpath
+cd $buildpath
+if [ "$device" == "a" -o "$device" == "adas" ]
+then
+    echo "./building for adas"
+    target_dist=$adas_target
+    ../adas_cmake.sh
 else
-../dms_cmake.sh
+    echo "./building for dms"
+    target_dist=$dms_target
+    ../dms_cmake.sh
 fi
 make
+cd ..
+
+outputpath=$output_folder`git_reversion`"/"
+mkdir -p $outputpath
+echo "generate target $outputpath"
+
+cp $buildtarget $outputpath$target_dist
+
